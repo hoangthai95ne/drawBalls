9a92322961cd9288f92014155f21244f2e3f3a0b
IM Hoang Thai <hoangthai95ne@gmail.com>
2015-12-15 09:32:36 +0700

    Initial Commit

---
+++ drawBalls/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  drawBalls
+//
+//  Created by Hoàng Thái on 12/15/15.
+//  Copyright © 2015 HOANGTHAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
